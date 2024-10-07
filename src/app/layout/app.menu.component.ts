85ca56a191e73caa1368799ff0460fda46090558
Amédée DERA <dera.amedee@gmail.com>
2024-10-07 10:08:46 +0000

    template crafting

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -17,36 +17,29 @@ export class AppMenuComponent implements OnInit {
             {
                 label: 'Tableau de bord',
                 items: [
-                    { label: 'Accueil', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
+                    { label: 'Accueil', icon: 'pi pi-fw pi-home', routerLink: ['/app'] }
                 ]
             },
             {
                 label: 'Modules',
                 items: [
-                    { label: 'Campagnes', icon: 'pi pi-fw pi-clone', routerLink: ['/pages/campagnes'] },
-                    { label: 'Expression de besoin', icon: 'pi pi-fw pi-list', routerLink: ['/pages/besoins'] },
-                    { label: 'Annonces', icon: 'pi pi-fw pi-bookmark', routerLink: ['/pages/annonces'] },
-                    { label: 'Niveau de stock', icon: 'pi pi-fw pi-box', routerLink: ['/pages/stock'] },
-                    { label: 'Notifications', icon: 'pi pi-fw pi-comment', routerLink: ['/uikit/message'] },
+                    { label: 'Actes', icon: 'pi pi-fw pi-clone', routerLink: ['/app/pages/actes'] },
+                    { label: 'Mes demandes', icon: 'pi pi-fw pi-list', routerLink: ['/app/pages/demandes'] },
+                    // { label: 'Notifications', icon: 'pi pi-fw pi-comment', routerLink: ['/app/pages/notifications'] },
                    
                 ]
             },
-            {
-                label: 'Configuration',
-                items: [
-                    { label: 'Type de campagnes', icon: 'pi pi-fw pi-eye', routerLink: ['/blocks'], badge: 'NEW' },
-                    { label: 'Catégorie de campagnes', icon: 'pi pi-fw pi-eye', routerLink: ['/blocks'], badge: 'NEW' },
-                    { label: 'Type d\'expresion de besoin', icon: 'pi pi-fw pi-eye', routerLink: ['/blocks'], badge: 'NEW' },
-                    { label: 'Type d\'annonces', icon: 'pi pi-fw pi-eye', url: ['https://www.primefaces.org/primeblocks-ng'], target: '_blank' },
-                ]
-            },
+            // {
+            //     label: 'Configuration',
+            //     items: [
+            //         { label: 'Type de campagnes', icon: 'pi pi-fw pi-eye', routerLink: ['/blocks'], badge: 'NEW' },
+            //     ]
+            // },
             {
                 label: 'Sécurité',
                 items: [
-                    { label: 'Profil', icon: 'pi pi-fw pi-user', routerLink: ['/pages/utilisateurs'] },
-                    { label: 'Utilisateurs ', icon: 'pi pi-fw pi-users', url: ['/pages/utilisateurs'], target: '_blank' },
-                    // { label: 'Groupes ', icon: 'pi pi-fw pi-user-lock', url: ['https://www.primefaces.org/primeflex/'], target: '_blank' },
-                    { label: 'Permissions ', icon: 'pi pi-fw pi-shield', url: ['/pages/utilisateurs'], target: '_blank' },
+                    { label: 'Profil', icon: 'pi pi-fw pi-user', routerLink: ['/app/pages/profil'] },
+                    // { label: 'Utilisateurs ', icon: 'pi pi-fw pi-users', url: ['/app/utilisateurs'], target: '_blank' },
                 ]
             }
         ];
