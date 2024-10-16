38f70419c5684a542ef58fd69b3810ce9bcac1d4
Amédée DERA <dera.amedee@gmail.com>
2024-10-16 02:09:55 +0000

    calendar field

--- src/app/demo/components/traitement/drtss/drtss.component.ts
+++ src/app/demo/components/traitement/drtss/drtss.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { DrtssService } from '../../../services/drtss.service';
 import { DemandeDrtss, DemandeDrtssResponse } from '../../../models/drtss';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-drtss',
@@ -32,7 +34,7 @@ export class TraitementDrtssComponent implements OnInit {
   attestationAnpeDate: Date = null;
   attestationCnssDate: Date = null;
 
-  constructor(private drtssService: DrtssService, private messageService: MessageService) {}
+  constructor(private drtssService: DrtssService, private messageService: MessageService, private router: Router) {}
 
   ngOnInit() {
     this.getDemandes();
@@ -79,23 +81,54 @@ export class TraitementDrtssComponent implements OnInit {
     this.messageService.add({ severity: 'info', summary: 'Succès', detail: 'Fichier téléchargé', life: 3000 });
   }
 
+  isFormValid(): boolean {
+    return !!this.attestationAnpeNumber 
+         && !!this.attestationAnpeDate 
+         && !!this.attestationCnssNumber 
+         && !!this.attestationCnssDate;
+  }
+
   processRequest() {
-    if (this.selectedRequest) {
+    if (!this.isFormValid()) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Erreur',
+        detail: 'Tous les champs sont obligatoires. Veuillez les remplir.',
+        life: 3000
+      });
+      return;
+    }
+    if (this.request) {
       const formData = new FormData();
       formData.append('attestationAnpeNumber', this.attestationAnpeNumber);
-      formData.append('attestationAnpeDate', this.attestationAnpeDate ? this.attestationAnpeDate.toISOString().split('T')[0] : '');
+      // formData.append('attestationAnpeDate', this.attestationAnpeDate ? this.attestationAnpeDate.toISOString(): '');
+      formData.append('attestationAnpeDate', this.attestationAnpeDate ? this.attestationAnpeDate.toISOString(): '');
       formData.append('attestationCnssNumber', this.attestationCnssNumber);
-      formData.append('attestationCnssDate', this.attestationCnssDate ? this.attestationCnssDate.toISOString().split('T')[0] : '');
+      formData.append('attestationCnssDate', this.attestationCnssDate ? this.attestationCnssDate.toISOString(): '');
+      // formData.append('attestationCnssDate', this.attestationCnssDate ? this.attestationCnssDate.toISOString(): '');
 
-      this.drtssService.approveRequest(this.selectedRequest.id, formData).subscribe(() => {
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Succès',
-          detail: 'Demande traitée et approuvée',
-          life: 3000
-        });
-        this.displayProcessModal = false;
-        this.selectedRequest = null;
+      this.drtssService.approveRequest(this.request.id, formData).subscribe({
+        next: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Succès',
+            detail: 'Demande traitée et approuvée',
+            life: 3000
+          });
+          this.displayProcessModal = false;
+          this.request = null;
+          setTimeout(() => {
+            this.router.navigate(['/app/traitement/drtss']); 
+          }, 500); 
+        },
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erreur',
+            detail: 'Une erreur est survenue lors de l\'approbation de la demande.',
+            life: 3000
+          });
+        }
       });
     }
   }
