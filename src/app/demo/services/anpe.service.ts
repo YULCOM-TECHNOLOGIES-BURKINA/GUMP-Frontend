8514008a8cca4f194cf8a384578d94ee9594cd4c
Amédée DERA <dera.amedee@gmail.com>
2024-11-06 11:45:15 +0000

    fix: historique demandes

--- src/app/demo/services/anpe.service.ts
+++ src/app/demo/services/anpe.service.ts
@@ -8,7 +8,7 @@ import { DemandeAnpe, DemandeAnpeResponse } from '../models/anpe';
 })
 export class AnpeService {
 
-  private apiUrl = 'http://54.37.13.176:8082/api/demandes'; 
+  private apiUrl = 'http://195.35.48.198:8080/api/demandes'; 
 
   constructor(private http: HttpClient) {}
 
