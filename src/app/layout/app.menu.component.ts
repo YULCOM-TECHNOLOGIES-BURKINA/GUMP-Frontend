eb578de1ea7c13200ee4ee6715df1a48185952fb
Amédée DERA <dera.amedee@gmail.com>
2024-11-20 03:44:07 +0000

    ajustement menu ADMIN

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -43,28 +43,28 @@ export class AppMenuComponent implements OnInit {
                     { label: 'Attestation AJE', icon: 'pi pi-fw pi-file', routerLink: ['/app/traitement/aje'], visible: this.userRole.includes('ADMIN')  || this.userRole.includes('TRESOR_AGENT') }
                 ]
             },
+            // {
+            //     label: 'Paramètres',
+            //     visible: this.userRole.includes('ADMIN') ,
+            //     items: [
+            //         { label: 'Informations générales', icon: 'pi pi-fw pi-user', routerLink: ['/app/pages/profil'] },
+            //         { label: 'Frais de traitement', icon: 'pi pi-fw pi-users', routerLink: ['/app/pages/utilisateurs']},
+            //         { label: 'Délais de traitement', icon: 'pi pi-fw pi-users', routerLink: ['/app/pages/utilisateurs']},
+            //         { label: 'En-têtes et pieds de page des actes', icon: 'pi pi-fw pi-users', routerLink: ['/app/pages/utilisateurs']},
+            //         { label: 'Signatures electroniques', icon: 'pi pi-fw pi-users', routerLink: ['/app/pages/utilisateurs']}
+            //     ]
+            // },
             {
                 label: 'Paramètres',
-                visible: this.userRole.includes('ADMIN') ,
-                items: [
-                    { label: 'Informations générales', icon: 'pi pi-fw pi-user', routerLink: ['/app/pages/profil'] },
-                    { label: 'Frais de traitement', icon: 'pi pi-fw pi-users', routerLink: ['/app/pages/utilisateurs']},
-                    { label: 'Délais de traitement', icon: 'pi pi-fw pi-users', routerLink: ['/app/pages/utilisateurs']},
-                    { label: 'En-têtes et pieds de page des actes', icon: 'pi pi-fw pi-users', routerLink: ['/app/pages/utilisateurs']},
-                    { label: 'Signatures electroniques', icon: 'pi pi-fw pi-users', routerLink: ['/app/pages/utilisateurs']}
-                ]
-            },
-            {
-                label: 'Paramètres',
-                visible: this.userRole.includes('DRTSS_AGENT'),
+                visible: this.userRole.includes('ADMIN') || this.userRole.includes('DRTSS_AGENT') || this.userRole.includes('TRESOR_AGENT'),
                 items: [
                     { label: 'Informations générales', icon: 'pi pi-fw pi-user', routerLink: ['/app/pages/profil'] },
                    // { label: 'Gestions Utilisateurs', icon: 'pi pi-fw pi-users', routerLink: ['/app/pages/utilisateurs'], visible: this.userRole.includes('DRTSS_AGENT') },
-                    { label: 'Gestions Utilisateurs DRTSS', icon: 'pi pi-fw pi-users', routerLink: ['/app/pages/utilisateurs-drtss/gestions'], visible: this.userRole.includes('DRTSS_AGENT') },
-                    { label: 'Signatures electroniques', icon: 'pi pi-fw pi-qrcode', routerLink: ['/app/pages/signature-electronique/signataire'], visible: this.userRole.includes('DRTSS_AGENT') },
-                    { label: 'Signer Attestations', icon: 'pi pi-fw pi-file-edit', routerLink: ['/app/pages/signature-electronique/sign_attestation'], visible: this.userRole.includes('DRTSS_AGENT') },
-                    { label: 'Paramétrage délais', icon: 'pi pi-fw pi-paperclip', routerLink: ['/app/pages/application-config'], visible:  this.userRole.includes('DRTSS_AGENT')},
-                    { label: 'Paramétrage délais', icon: 'pi pi-fw pi-paperclip', routerLink: ['/app/pages/application-config'], visible:  this.userRole.includes('TRESOR_AGENT')}
+                    { label: 'Gestions Utilisateurs DRTSS', icon: 'pi pi-fw pi-users', routerLink: ['/app/pages/utilisateurs-drtss/gestions'], visible: this.userRole.includes('ADMIN') || this.userRole.includes('DRTSS_AGENT') },
+                    { label: 'Signatures electroniques', icon: 'pi pi-fw pi-qrcode', routerLink: ['/app/pages/signature-electronique/signataire'], visible: this.userRole.includes('ADMIN') || this.userRole.includes('DRTSS_AGENT') },
+                    { label: 'Signer Attestations', icon: 'pi pi-fw pi-file-edit', routerLink: ['/app/pages/signature-electronique/sign_attestation'], visible: this.userRole.includes('ADMIN') || this.userRole.includes('DRTSS_AGENT') },
+                    { label: 'Paramétrage délais', icon: 'pi pi-fw pi-paperclip', routerLink: ['/app/pages/application-config'], visible:  this.userRole.includes('ADMIN') || this.userRole.includes('DRTSS_AGENT')},
+                    { label: 'Paramétrage délais', icon: 'pi pi-fw pi-paperclip', routerLink: ['/app/pages/application-config'], visible:  this.userRole.includes('ADMIN') || this.userRole.includes('TRESOR_AGENT')}
                 ]
             },
             {
